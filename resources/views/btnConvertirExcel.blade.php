8023aa748ffc01cb1b9a43610a1b5574400cff06
felipeCR <cifuentesrfelipe@gmail.com>
2016-09-02 19:02:00 -0500

    Clase css del btn

--- resources/views/btnConvertirExcel.blade.php
+++ resources/views/btnConvertirExcel.blade.php
@@ -1,11 +1,11 @@
 
 Para descargar oprima aquí, aparecerá un botón que al oprimirlo descargará el archivo en formato .xls 
-<button id="btndescargainf">
+<button id="btndescargainf" class="btn btn-default">
     Iniciar Proceso
 </button>
 <form action="/descargaExcel" method="post" id="formDescargas">
     {{csrf_field()}}
-    <input type="hidden" name="arrayData">
-    <input type="submit" value="Descargar Informe en formato .xls" >
+    <input type="hidden" name="arrayData" >
+    <input type="submit" value="Descargar Informe en formato .xls" class="btn btn-danger">
 </form>
 
